43e6a21923428c4e1928444d3cc4eddecc77f3ea
Michelle Seguil <michelle@laboratoria.la>
2018-03-22 12:49:46 -0500

    add base

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,1 @@
-var gulp = require('gulp'),
-  uglify = require('gulp-uglify'),
-  concat = require('gulp-concat');
-
-gulp.task('js', function () {
-  return gulp.src('src/js/*.js')
-    .pipe(concat('app.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('dist/js/'))
-});
+var gulp = require('gulp');
