edd4c940a743c234224a0fd6b9481c5806757d69
Michelle Seguil <michelle@laboratoria.la>
2018-03-23 09:06:34 -0500

    add task css

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var babel = require('gulp-babel');
+var minifycss =require('gulp-minify-css');
 
 gulp.task('js', function () {
   return gulp.src('src/js/*.js')
@@ -9,4 +10,11 @@ gulp.task('js', function () {
     .pipe(babel({ presets: ['env'] }))    
     .pipe(uglify())
     .pipe(gulp.dest('dist/js/'))
+});
+
+gulp.task('css', function () {
+  return gulp.src('src/css/*.css')
+    .pipe(concat('main.min.css'))
+    .pipe(minifycss())
+    .pipe(gulp.dest('dist/css/'))
 });
